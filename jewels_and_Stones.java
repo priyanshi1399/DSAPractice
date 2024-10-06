eca39c9838be9a66ecfea91b4c3ed5a5644eb78d
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-06 17:08:37 +0530

    Solved Problem jewels and Stones

---
+++ jewels_and_Stones.java
@@ -0,0 +1,26 @@
+public class jewels_and_Stones {
+
+
+    private static int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(int i=0;i<stones.length();i++) {
+            if (jewels.contains(String.valueOf(stones.charAt(i)))) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+
+    public static void main(String[] args){
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+        int k=numJewelsInStones(jewels,stones);
+        System.out.println(k);
+
+        //o/p=3
+    }
+
+
+}
