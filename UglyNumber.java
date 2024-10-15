fea4e6b9e1c9527c6b489e235d0ed1fcd5d1f6af
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-15 21:13:55 +0530

    UglyNumber(Prime factor limited to 2,3,5)

---
+++ UglyNumber.java
@@ -0,0 +1,24 @@
+public class UglyNumber {
+
+    public static boolean uglyNumber(int n ) {
+        while (n <= 0) {
+            return false;
+        }
+        while(n%2==0){
+            n=n/2;
+        }
+        while(n%3==0){
+            n=n/3;
+        }
+        while(n%5==0){
+            n=n/5;
+        }
+        return (n==1);
+    }
+
+    public static void main(String[] args) {
+        int n = 14;
+        boolean res=uglyNumber(n);
+        System.out.println(res);
+    }
+}
