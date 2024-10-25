2323f12b737604fa4b250e53d6e1e9db97e41f9b
priyanshi1399 <priyanshitomar001@gmail.com>
2024-10-26 01:55:58 +0530

    CountGoodTriplets

---
+++ CountGoodTriplets.java
@@ -0,0 +1,25 @@
+public class CountGoodTriplets {
+
+    public static int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                for(int k=j+1;k<arr.length;k++){
+                    if(Math.abs(arr[i]-arr[j])<=a && Math.abs(arr[j]-arr[k])<=b && Math.abs(arr[i]-arr[k])<=c){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int [] arr = {3,0,1,1,9,7};
+        int a = 7;
+        int b = 2;
+        int c = 3;
+        int count=countGoodTriplets(arr,a,b,c);
+        System.out.println(count);
+    }
+}
