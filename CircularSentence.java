99cdf03e9f2f40ab67ba18794f45dafbf93b4928
priyanshi1399 <priyanshitomar001@gmail.com>
2024-11-07 23:55:07 +0530

    Circular Sentence

---
+++ CircularSentence.java
@@ -0,0 +1,28 @@
+public class CircularSentence {
+
+public static void main(String [] args){
+    String sentence = "leetcode exercises sound delightful";
+    isCircularSentence(sentence);
+}
+
+    public static boolean isCircularSentence(String sentence) {
+        int n=sentence.length();
+        boolean flag=false;
+
+        int i=0;
+        int j=-1;
+        for(;i<n;i++){
+            if(sentence.charAt(i)==' '){
+                j=i;
+                if(sentence.charAt(j-1)!=sentence.charAt(j+1)){
+                    return false;
+                }
+            }
+        }
+        if(sentence.charAt(0)==sentence.charAt(n-1)){
+            flag=true;
+        }
+
+        return flag;
+    }
+}
